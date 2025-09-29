7bab813464abadc6b4be16bce768040002d38db1
thang.le <thang.le@gmail.com>
2025-09-29 13:37:07 +0700

    refactor

--- src/main/java/com/example/userservice/infrastructure/middleware/AuthMiddleware.java
+++ src/main/java/com/example/userservice/infrastructure/middleware/AuthMiddleware.java
@@ -3,25 +3,31 @@ package com.example.userservice.infrastructure.middleware;
 import com.example.userservice.service.CustomClaim;
 import com.example.userservice.service.JwtService;
 import io.javalin.http.Handler;
+import io.javalin.http.HandlerType;
 import io.javalin.http.HttpResponseException;
 
 public class AuthMiddleware {
     public static Handler authenticate = ctx -> {
         String authHeader = ctx.header("Authorization");
-        
+
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             throw new HttpResponseException(401, "Missing or invalid token");
         }
 
         String token = authHeader.substring(7); // Remove "Bearer " prefix
-        
+
         if (!JwtService.validateToken(token)) {
             throw new HttpResponseException(401, "Invalid or expired token");
         }
 
         CustomClaim claim = JwtService.getClaimFromToken(token);
 
-        if (!ctx.path().equals("/me")  && !claim.getRole().equals("ADMIN")) {
+        // bypass authentication for create a new user
+        if (ctx.path().equals("/users") && ctx.method() == HandlerType.POST) {
+            return;
+        }
+
+        if (!ctx.path().equals("/me") && !claim.getRole().equals("ADMIN")) {
             throw new HttpResponseException(403, "Insufficient privileges");
         }
 
